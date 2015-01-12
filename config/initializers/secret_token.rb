f656b4656ba4f259aeb9161d3a8ec1313d351680
Corrie Hibberd <corrie@sandersteadbells.org.uk>
2015-01-12 22:12:26 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ResidentsApp::Application.config.secret_token = '2a6f1e102f5bcb56bae1b81e02697e103f33a97b11f4489913244e380da8a6ec28c0e588533c86bd02ed6496014673c8aeb54c60ca185433a8bef2df3e7f1db2'
